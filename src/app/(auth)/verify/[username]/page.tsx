7de917bd982c4b403b80239fc69ea0f83e639f4b
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-07-12 14:54:05 +0530

    changed opt input to shadcn otp-input

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { useToast } from '@/components/ui/use-toast'
 import { verifySchema } from '@/schemas/verifySchema'
 import { ApiResponse } from '@/types/ApiResponse'
@@ -12,6 +12,7 @@ import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { Button } from '@/components/ui/button'
 import { Loader2 } from 'lucide-react'
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp"
 
 const VerifyAccount = () => {
      const router = useRouter()
@@ -53,7 +54,7 @@ const VerifyAccount = () => {
                <div className="w-full max-w-lg p-8 space-y-8 bg-white rounded-lg shadow-md">
                     <div className="text-center">
                          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Verify Your Account</h1>
-                         <p className="mb-4">Enter the verification code sent to your email.</p>
+                         <p className="mb-4">Please check your email.</p>
                     </div>
                     <Form {...form}>
                          <form className='space-y-6' onSubmit={form.handleSubmit(onSubmit)}>
@@ -64,8 +65,20 @@ const VerifyAccount = () => {
                                         <FormItem>
                                              <FormLabel>Verification Code</FormLabel>
                                              <FormControl>
-                                                  <Input placeholder="OTP" {...field} />
+                                                  <InputOTP maxLength={6} {...field}>
+                                                       <InputOTPGroup>
+                                                            <InputOTPSlot index={0} />
+                                                            <InputOTPSlot index={1} />
+                                                            <InputOTPSlot index={2} />
+                                                            <InputOTPSlot index={3} />
+                                                            <InputOTPSlot index={4} />
+                                                            <InputOTPSlot index={5} />
+                                                       </InputOTPGroup>
+                                                  </InputOTP>
                                              </FormControl>
+                                             <FormDescription>
+                                                  Please enter the one-time password sent to your email.
+                                             </FormDescription>
                                              <FormMessage />
                                         </FormItem>
                                    )}
